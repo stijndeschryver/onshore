b1d2cb2d3a3becdfa3a68fed29adcf71f347e939
stijndeschryver <stijndeschryver@icloud.com>
2025-01-30 08:39:07 +0100

    snap-container

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import './Home.css';
 
 export const Home = () => {
   return (
-    <div className="sticky-container">
+    <div className="snap-container">
       <section id="home">
         <div className="content">
           <h1>Home</h1>
