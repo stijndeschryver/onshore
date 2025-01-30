76d842a6047189a05706e057465cd3796b7da09b
stijndeschryver <stijndeschryver@icloud.com>
2025-01-30 08:37:52 +0100

    snap scrolling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,12 @@ import './Home.css';
 
 export const Home = () => {
   return (
-    <section id="home">
-      <div className="content">
-        <h1>Home</h1>
-      </div>
-    </section>
+    <div className="sticky-container">
+      <section id="home">
+        <div className="content">
+          <h1>Home</h1>
+        </div>
+      </section>
+    </div>
   );
 };
