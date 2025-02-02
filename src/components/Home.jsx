ef694a0e55794be7d1767c5b8e8e6e2c18040fe7
stijndeschryver <stijndeschryver@icloud.com>
2025-02-02 16:19:24 +0100

    fix home logo position

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,23 @@
+// Home.jsx
+import { useEffect, useRef } from 'react';
 import './Home.css';
 import { Logo } from './svg/Logo';
 
 export const Home = () => {
+  const spacerRef = useRef(null);
+
+  useEffect(() => {
+    if (spacerRef.current) {
+      // Get the initial height when the component mounts
+      const initialHeight = spacerRef.current.offsetHeight;
+      // Set the fixed height
+      spacerRef.current.style.height = `${initialHeight}px`;
+    }
+  }, []);
+
   return (
     <>
-      <div className="home-spacer" />
+      <div className="home-spacer" ref={spacerRef} />
       <section id="home">
         <div className="content">
           <Logo className="Logo" />
