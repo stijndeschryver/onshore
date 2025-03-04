bf70341317a47c30f06d2c04016071a0220677f1
stijndeschryver <stijndeschryver@icloud.com>
2025-03-04 16:52:14 +0100

    case tiles static

--- src/components/Cases.jsx
+++ src/components/Cases.jsx
@@ -1,32 +1,88 @@
 import './Cases.css';
 import { Wave } from './svg/Wave.jsx';
 import { WaveCorner } from './svg/WaveCorner.jsx';
-import { useCases } from '../hooks/useCases.js';
-import { VideoWithLoading } from './Video.jsx';
 
 export const Cases = () => {
-  const { data, error } = useCases();
-
+  const cards = [
+    {
+      id: 1,
+      title: 'Static to video',
+      subtitle: 'BASE',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        ' Vivamus pretium lacus a massa dapibus molestie. Donec accumsan auctor tristique. Integer pellentesque, sem ut pulvinar euismod, arcu augue pulvinar felis, et feugiat augue tellus quis ipsum. Quisque dapibus massa consequat lorem mattis placerat. Praesent porta, justo vitae molestie rhoncus, turpis ex consequat massa, sed semper elit dolor sit amet diam. Cras tristique mi at ex dictum malesuada. Aliquam sed justo sem. Sed rutrum, ligula ac maximus eleifend, ante elit elementum justo, a dictum diam libero non justo.',
+      className: 'card-large bg-card-1',
+    },
+    {
+      id: 2,
+      title: 'Static AI image generation',
+      subtitle: 'TotalEnergies',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        ' Vivamus pretium lacus a massa dapibus molestie. Donec accumsan auctor tristique. Integer pellentesque, sem ut pulvinar euismod, arcu augue pulvinar felis, et feugiat augue tellus quis ipsum. Quisque dapibus massa consequat lorem mattis placerat. Praesent porta, justo vitae molestie rhoncus, turpis ex consequat massa, sed semper elit dolor sit amet diam. Cras tristique mi at ex dictum malesuada. Aliquam sed justo sem. Sed rutrum, ligula ac maximus eleifend, ante elit elementum justo, a dictum diam libero non justo.',
+      className: 'card-wide bg-card-2',
+    },
+    {
+      id: 3,
+      title: 'Static to video',
+      subtitle: 'Richa',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        ' Vivamus pretium lacus a massa dapibus molestie. Donec accumsan auctor tristique. Integer pellentesque, sem ut pulvinar euismod, arcu augue pulvinar felis, et feugiat augue tellus quis ipsum. Quisque dapibus massa consequat lorem mattis placerat. Praesent porta, justo vitae molestie rhoncus, turpis ex consequat massa, sed semper elit dolor sit amet diam. Cras tristique mi at ex dictum malesuada. Aliquam sed justo sem. Sed rutrum, ligula ac maximus eleifend, ante elit elementum justo, a dictum diam libero non justo.',
+      className: 'card-wide bg-card-3',
+    },
+    {
+      id: 4,
+      title: 'Avatar generation (Synthetic)',
+      subtitle: 'BNP Paribas Fortis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        ' Vivamus pretium lacus a massa dapibus molestie. Donec accumsan auctor tristique. Integer pellentesque, sem ut pulvinar euismod, arcu augue pulvinar felis, et feugiat augue tellus quis ipsum. Quisque dapibus massa consequat lorem mattis placerat. Praesent porta, justo vitae molestie rhoncus, turpis ex consequat massa, sed semper elit dolor sit amet diam. Cras tristique mi at ex dictum malesuada. Aliquam sed justo sem. Sed rutrum, ligula ac maximus eleifend, ante elit elementum justo, a dictum diam libero non justo.',
+      className: 'card-wide bg-card-4',
+    },
+    {
+      id: 5,
+      title: 'Video with added animation',
+      subtitle: 'BNP Paribas Fortis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        ' Vivamus pretium lacus a massa dapibus molestie. Donec accumsan auctor tristique. Integer pellentesque, sem ut pulvinar euismod, arcu augue pulvinar felis, et feugiat augue tellus quis ipsum. Quisque dapibus massa consequat lorem mattis placerat. Praesent porta, justo vitae molestie rhoncus, turpis ex consequat massa, sed semper elit dolor sit amet diam. Cras tristique mi at ex dictum malesuada. Aliquam sed justo sem. Sed rutrum, ligula ac maximus eleifend, ante elit elementum justo, a dictum diam libero non justo.',
+      className: 'card-large bg-card-5',
+    },
+    {
+      id: 6,
+      title: 'Prompted video',
+      subtitle: 'Telenet One',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        ' Vivamus pretium lacus a massa dapibus molestie. Donec accumsan auctor tristique. Integer pellentesque, sem ut pulvinar euismod, arcu augue pulvinar felis, et feugiat augue tellus quis ipsum. Quisque dapibus massa consequat lorem mattis placerat. Praesent porta, justo vitae molestie rhoncus, turpis ex consequat massa, sed semper elit dolor sit amet diam. Cras tristique mi at ex dictum malesuada. Aliquam sed justo sem. Sed rutrum, ligula ac maximus eleifend, ante elit elementum justo, a dictum diam libero non justo.',
+      className: 'card-wide bg-card-6',
+    },
+    {
+      id: 7,
+      title: 'Banners',
+      subtitle: 'Doccle',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        ' Vivamus pretium lacus a massa dapibus molestie. Donec accumsan auctor tristique. Integer pellentesque, sem ut pulvinar euismod, arcu augue pulvinar felis, et feugiat augue tellus quis ipsum. Quisque dapibus massa consequat lorem mattis placerat. Praesent porta, justo vitae molestie rhoncus, turpis ex consequat massa, sed semper elit dolor sit amet diam. Cras tristique mi at ex dictum malesuada. Aliquam sed justo sem. Sed rutrum, ligula ac maximus eleifend, ante elit elementum justo, a dictum diam libero non justo.',
+      className: 'card-full-width bg-card-7',
+    },
+  ];
   return (
     <section id="cases">
       <Wave index={1} />
       <Wave index={2} />
       <div className="content">
-        {error ? (
-          <div>Error: {error}</div>
-        ) : (
-          data.map((caseItem) => (
-            <VideoWithLoading key={caseItem.id} src={caseItem.video_uri} />
-          ))
-        )}
-        <p className="cases-description">
-          We create the stuff that makes your digital campaigns pop out. Whether
-          it’s display ads, social content, online video, animations, emails,
-          mini-sites, or (D)OOH.
-          <strong>
-            You focus on the big ideas, we’ll take care of the rest
-          </strong>
-        </p>
+        <div className="masonry-grid">
+          {cards.map((card) => (
+            <div key={card.id} className={`card ${card.className}`}>
+              <div className="card-content">
+                <div className="card-title">{card.title}</div>
+                <div className="card-subtitle">{card.subtitle}</div>
+              </div>
+            </div>
+          ))}
+        </div>
         <div className="wave-extension-left"></div>
         <WaveCorner />
         <div className="wave-extension-right"></div>
